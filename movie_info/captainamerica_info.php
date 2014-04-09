3e07e236c9fa421dd175aa067fcc1dc31320b16d
slaytex <chris@chrisjacobs.com>
2014-04-09 00:30:13 -0700

    fixed spelling of captainamerica

--- movie_info/captainamerica_info.php
+++ movie_info/captainamerica_info.php
@@ -1,7 +1,7 @@
 	<div id="content">
 
 				<div class="detail_image full">
-			    	<video src="http://slash_media/hls/trailers/captainamerica/output.m3u8" width="640" height="360" preload="none" controls poster="http://slash_media/hls/trailers/catainamerica/poster.png"></video>
+			    	<video src="http://slash_media/hls/trailers/captainamerica/output.m3u8" width="640" height="360" preload="none" controls poster="http://slash_media/hls/trailers/captainamerica/poster.png"></video>
 			    </div>
 
 			    <div class="detail_title">
