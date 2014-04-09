fd023989065b1d49b74f9d1994d34c83f0f37b9c
slaytex <chris@chrisjacobs.com>
2014-04-08 23:47:08 -0700

    changed width to 100% and height to auto

--- movie_info/bears_info.php
+++ movie_info/bears_info.php
@@ -1,7 +1,7 @@
 	<div id="content">
 
 				<div class="detail_image full">
-			    	<video src="http://slash_media/hls/trailers/bears/output.m3u8" width="640" height="360" preload="none" controls poster="http://slash_media/hls/trailers/bears/poster.png"></video>
+			    	<video src="http://slash_media/hls/trailers/bears/output.m3u8" width="100%" height="auto" preload="none" controls poster="http://slash_media/hls/trailers/bears/poster.png"></video>
 			    </div>
 
 			    <div class="detail_title">
